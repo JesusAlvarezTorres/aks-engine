2ca7c80ff4d64a1d97d412e1fd13a33060509576
JesusAlvarezTorres <jesusa@microsoft.com>
2020-07-08 10:41:07 -0700

    Adding 1.17.8 support for Azure Stack

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -224,6 +224,7 @@ var AllKubernetesSupportedVersionsAzureStack = map[string]bool{
 	"1.17.5":  false,
 	"1.17.6":  false,
 	"1.17.7":  false,
+	"1.17.8":  true,
 }
 
 // AllKubernetesWindowsSupportedVersionsAzureStack maintain a set of available k8s Windows versions in aks-engine on Azure Stack
@@ -239,6 +240,7 @@ var AllKubernetesWindowsSupportedVersionsAzureStack = map[string]bool{
 	"1.17.5":  false,
 	"1.17.6":  false,
 	"1.17.7":  false,
+	"1.17.8":  true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
